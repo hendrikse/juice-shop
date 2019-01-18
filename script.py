1ec10d8334cd632489d03fdfa7feef00f3b5c991
nikolaifrankeedu <46650029+nikolaifrankeedu@users.noreply.github.com>
2019-01-18 09:51:33 +0100

    Add files via upload

---
+++ script.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.proxy import Proxy, ProxyType
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+myProxy = "172.17.0.4:8090"
+proxy = Proxy({
+    'proxyType': ProxyType.MANUAL,
+    'httpProxy': myProxy,
+    'sslProxy': myProxy,
+    'fpProxy': myProxy,
+    'noProxy': ''
+})
+
+
+driver = webdriver.Remote(
+    proxy=proxy,
+    command_executor='http://127.0.0.1:4444/wd/hub',
+    desired_capabilities=DesiredCapabilities.FIREFOX)
+
+driver.get("https://bbc.com")
+print(driver.title)
+driver.close();
